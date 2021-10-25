224b035cdcca328d2264ae1000b5dcd21fde5b4c
AdamiSheff <adamisheff@gmail.com>
2021-10-25 13:24:29 -0400

    Updated App.jsx to display ClusterConnect. Updated ClusterConnect css to center and add background colors

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,13 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { 
+  BrowserRouter as Router, 
+  Switch, 
+  Route, 
+  Link } from "react-router-dom";
 
 // Import components
 import Sidebar from "../components/Sidebar.jsx";
-// import ClusterConnect from '../components/ClusterConnect.jsx';
+import ClusterConnect from '../components/ClusterConnect.jsx';
 import Dashboard from "./Dashboard.jsx";
 import Nodes from "./Nodes.jsx";
 import Pods from "./Pods.jsx";
@@ -14,14 +18,19 @@ import Pods from "./Pods.jsx";
 import "../assets/css/App.css";
 
 export default function App() {
+
+  const [connected, useConnected] = useState(false);
   return (
     <div>
-      <Sidebar />
-      <h1>Hello Team Kr8s!</h1>
-      <p>Now for the fun part...</p>
+      {/* Display Sidebar only if we are not in the root path ('/') */}
+      {/* <Sidebar /> */}
+
       <Router>
         <div>
           <Switch>
+            <Route exact="/">
+              <ClusterConnect clusters={["Cluster1", "Cluster2"]}/>
+            </Route>
             <Route path="/dash">
               <Dashboard />
             </Route>
