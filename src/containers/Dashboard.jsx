241d93cfe076cc966551e4207804fee25c298ccb
AdamiSheff <adamisheff@gmail.com>
2021-11-06 14:36:31 -0400

    Working on dashboard styling

--- src/containers/Dashboard.jsx
+++ src/containers/Dashboard.jsx
@@ -15,65 +15,57 @@ export default function Dashboard(props) {
         ]}
       />
 
-      <div className={style.speedoBoxes}>
-        <div className={style.speedoes}>
+      <div id={style.speedometers}>
+        <iframe
+          className={style.speedo}
+          src="http://localhost:32000/d-solo/sO3bv1Fnz/kr8s-dashboard?orgId=1&theme=dark&panelId=46"
+          frameBorder="1"
+        ></iframe>
+        <iframe
+          className={style.speedo}
+          src="http://localhost:32000/d-solo/sO3bv1Fnz/kr8s-dashboard?orgId=1&theme=dark&panelId=42"
+          frameBorder="1"
+        ></iframe>
+        <iframe
+          className={style.speedo}
+          src="http://localhost:32000/d-solo/sO3bv1Fnz/kr8s-dashboard?orgId=1&theme=dark&panelId=40"
+          frameBorder="1"
+        ></iframe>
+        <iframe
+          className={style.speedo}
+          src="http://localhost:32000/d-solo/sO3bv1Fnz/kr8s-dashboard?orgId=1&theme=dark&panelId=44"
+          frameBorder="1"
+        ></iframe>
+      </div>
+
+      <div id={style.linegraphs}>
+        {/* using dashboard 12006 */}
+        <div class={style.linegraphRow}>
           <iframe
-            src="http://localhost:32000/d-solo/sO3bv1Fnz/kr8s-dashboard?orgId=1&theme=dark&panelId=46"
-            width="280"
-            height="200"
-            frameBorder="0"
+            className={style.linegraph}
+            src="http://localhost:32000/d-solo/sO3bv1Fnz/kr8s-dashboard?orgId=1&theme=dark&panelId=16"
+            frameBorder="1"
           ></iframe>
           <iframe
-            src="http://localhost:32000/d-solo/sO3bv1Fnz/kr8s-dashboard?orgId=1&theme=dark&panelId=42"
-            width="280"
-            height="200"
-            frameBorder="0"
-          ></iframe>
-          <iframe
-            src="http://localhost:32000/d-solo/sO3bv1Fnz/kr8s-dashboard?orgId=1&theme=dark&panelId=40"
-            width="280"
-            height="200"
-            frameBorder="0"
-          ></iframe>
-          <iframe
-            src="http://localhost:32000/d-solo/sO3bv1Fnz/kr8s-dashboard?orgId=1&theme=dark&panelId=44"
-            width="280"
-            height="200"
-            frameBorder="0"
+            class={style.linegraph}
+            src="http://localhost:32000/d-solo/sO3bv1Fnz/kr8s-dashboard?orgId=1&theme=dark&panelId=18"
+            frameBorder="1"
           ></iframe>
         </div>
 
-        <div className={style.speedoes}>
-          {/* <Speedometer src={apiServer} />
-          <Speedometer src={scheduler} />
-          <Speedometer src={controller} />
-          <Speedometer src={etcd} /> */}
-          {/* using dashboard 12006 */}
+        <div className={style.linegraphRow}>
           <iframe
-            src="http://localhost:32000/d-solo/sO3bv1Fnz/kr8s-dashboard?orgId=1&theme=dark&panelId=16"
-            width="280"
-            height="180"
-            frameBorder="0"
-          ></iframe>
-          <iframe
-            src="http://localhost:32000/d-solo/sO3bv1Fnz/kr8s-dashboard?orgId=1&theme=dark&panelId=18"
-            width="280"
-            height="180"
-            frameBorder="0"
-          ></iframe>
-          <iframe
+            className={style.linegraph}
             src="http://localhost:32000/d-solo/sO3bv1Fnz/kr8s-dashboard?orgId=1&theme=dark&panelId=20"
-            width="280"
-            height="180"
-            frameBorder="0"
+            frameBorder="1"
           ></iframe>
           <iframe
+            claclassNamess={style.linegraph}
             src="http://localhost:32000/d-solo/sO3bv1Fnz/kr8s-dashboard?orgId=1&theme=dark&panelId=22"
-            width="280"
-            height="180"
-            frameBorder="0"
+            frameBorder="1"
           ></iframe>
         </div>
+
       </div>
     </div>
   );
