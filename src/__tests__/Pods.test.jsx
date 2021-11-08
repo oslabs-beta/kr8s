c22969394f6c8984f5622e025bc5089a12e41a74
Duke Lee <leeduke90@gmail.com>
2021-11-08 12:45:34 -0500

    write more tests for pods component

--- src/__tests__/Pods.test.jsx
+++ src/__tests__/Pods.test.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 
-// import App from "../containers/App.jsx";
 import Pods from "../containers/Pods.jsx";
-import Banner from "../components/Banner.jsx";
 
 import { shallow, mount } from "enzyme";
 
@@ -35,17 +33,28 @@ describe("test pods page functionality", () => {
       (succeededPods = 0);
   });
   test("test to see if pod status increment function is working correctly", () => {
-    expect(wrapper.find(Banner).props().items[0].value).toBe(1);
+    // console.log(wrapper.find("Banner").props())
+    expect(wrapper.find("Banner").props().items[0].value).toBe(1);
+    expect(wrapper.find("Banner").props().items[1].value).toBe(0);
+    expect(wrapper.find("Banner").props().items[2].value).toBe(0);
+    expect(wrapper.find("Banner").props().items[3].value).toBe(0);
+    expect(wrapper.find("Banner").props().items[4].value).toBe(0);
   });
-  test("see if it works2", () => {
-    expect(2).toBe(2);
+  test("test to see if render 1 Banner component in Pods page", () => {
+    expect(wrapper.find("List")).toHaveLength(1);
   });
-});
-describe("testing the test2", () => {
-  test("see if it works3", () => {
-    expect(1).toBe(1);
+  test("test to see if render 5 iframes in Pods page", () => {
+    expect(wrapper.find("iframe")).toHaveLength(5);
   });
-  test("see if it works4", () => {
-    expect(2).toBe(2);
+  test("test to see if render 1 List component in Pods page", () => {
+    expect(wrapper.find("List")).toHaveLength(1);
+  });
+  test("test to see if there are 6 list headers passed to List component", () => {
+    console.log(wrapper.find("List").props().listValueHeaders);
+    expect(wrapper.find("List").props().listValueHeaders).toHaveLength(6);
+  });
+  test("test to see if list reroute prop is truthy", () => {
+    console.log(wrapper.find("List").props().reroute);
+    expect(wrapper.find("List").props().reroute).toBeTruthy();
   });
 });
