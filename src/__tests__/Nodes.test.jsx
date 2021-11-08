d9f553adfa837f4b326d003517a837bb64a9d1c9
Duke Lee <leeduke90@gmail.com>
2021-11-08 15:27:21 -0500

    finish nodes tests and update pods tests

--- src/__tests__/Nodes.test.jsx
+++ src/__tests__/Nodes.test.jsx
@@ -5,56 +5,58 @@ import Nodes from "../containers/Nodes.jsx";
 import { shallow, mount } from "enzyme";
 
 describe("test to see if Nodes page is rendering and functioning as expected", () => {
-  let wrapper, runningPods, pendingPods, failedPods, unknownPods, succeededPods;
-  const pods = [
+  let wrapper;
+  const nodes = [
     {
+      metadata: {
+        name: "testNode",
+      },
       status: {
-        phase: "Running",
         conditions: [
-          { status: true },
-          { status: true },
-          { status: true },
+          { status: false },
+          { status: false },
+          { status: false },
           { status: true },
         ],
-        containerStatuses: true,
       },
-      metadata: { name: null },
-      spec: { containers: [1, 2, 3] },
     },
   ];
   beforeAll(() => {
-    wrapper = mount(<Pods pods={pods} />);
+    wrapper = mount(<Nodes nodes={nodes} />);
   });
   beforeEach(() => {
-    (runningPods = 0),
-      (pendingPods = 0),
-      (failedPods = 0),
-      (unknownPods = 0),
-      (succeededPods = 0);
+    const nodeValues = {
+      node: null,
+      memoryPressure: null,
+      diskPressure: null,
+      pidPressure: null,
+      ready: null,
+    };
   });
-  test("pod status incrementing functionality", () => {
-    // console.log(wrapper.find("Banner").props())
+  test("number of nodes count incrementing functionality", () => {
     expect(wrapper.find("Banner").props().items[0].value).toBe(1);
-    expect(wrapper.find("Banner").props().items[1].value).toBe(0);
-    expect(wrapper.find("Banner").props().items[2].value).toBe(0);
-    expect(wrapper.find("Banner").props().items[3].value).toBe(0);
-    expect(wrapper.find("Banner").props().items[4].value).toBe(0);
   });
-  test("render 1 Banner component in Pods page", () => {
+  test("render 1 Banner component in Nodes page", () => {
+    expect(wrapper.find("Banner")).toHaveLength(1);
+  });
+  test("render 3 iframes in Nodes page", () => {
+    expect(wrapper.find("iframe")).toHaveLength(3);
+  });
+  test("render 1 List component in Nodes page", () => {
     expect(wrapper.find("List")).toHaveLength(1);
   });
-  test("render 5 iframes in Pods page", () => {
-    expect(wrapper.find("iframe")).toHaveLength(5);
+  test("5 list headers passed to List component", () => {
+    expect(wrapper.find("List").props().listValueHeaders).toHaveLength(5);
   });
-  test("render 1 List component in Pods page", () => {
-    expect(wrapper.find("List")).toHaveLength(1);
+  test("list reroute prop is falsy", () => {
+    expect(wrapper.find("List").props().reroute).toBeFalsy();
   });
-  test("6 list headers passed to List component", () => {
-    console.log(wrapper.find("List").props().listValueHeaders);
-    expect(wrapper.find("List").props().listValueHeaders).toHaveLength(6);
-  });
-  test("list reroute prop is truthy", () => {
-    console.log(wrapper.find("List").props().reroute);
-    expect(wrapper.find("List").props().reroute).toBeTruthy();
+  test("list listValue prop object", () => {
+    expect(
+      wrapper.find("List").props().listValue[0].memoryPressure
+    ).toBeFalsy();
+    expect(wrapper.find("List").props().listValue[0].diskPressure).toBeFalsy();
+    expect(wrapper.find("List").props().listValue[0].pidPressure).toBeFalsy();
+    expect(wrapper.find("List").props().listValue[0].ready).toBeTruthy();
   });
 });
