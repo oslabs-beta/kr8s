63f1b0cf695ce3cbd853b838cba6c49b59cb5bd3
Duke Lee <leeduke90@gmail.com>
2021-11-08 13:15:36 -0500

    finish pods page testing

--- src/__tests__/Pods.test.jsx
+++ src/__tests__/Pods.test.jsx
@@ -4,7 +4,7 @@ import Pods from "../containers/Pods.jsx";
 
 import { shallow, mount } from "enzyme";
 
-describe("test pods page functionality", () => {
+describe("test to see if Pods page is rendering and functioning as expected", () => {
   let wrapper, runningPods, pendingPods, failedPods, unknownPods, succeededPods;
   const pods = [
     {
@@ -32,7 +32,7 @@ describe("test pods page functionality", () => {
       (unknownPods = 0),
       (succeededPods = 0);
   });
-  test("test to see if pod status increment function is working correctly", () => {
+  test("pod status incrementing functionality", () => {
     // console.log(wrapper.find("Banner").props())
     expect(wrapper.find("Banner").props().items[0].value).toBe(1);
     expect(wrapper.find("Banner").props().items[1].value).toBe(0);
@@ -40,20 +40,20 @@ describe("test pods page functionality", () => {
     expect(wrapper.find("Banner").props().items[3].value).toBe(0);
     expect(wrapper.find("Banner").props().items[4].value).toBe(0);
   });
-  test("test to see if render 1 Banner component in Pods page", () => {
+  test("render 1 Banner component in Pods page", () => {
     expect(wrapper.find("List")).toHaveLength(1);
   });
-  test("test to see if render 5 iframes in Pods page", () => {
+  test("render 5 iframes in Pods page", () => {
     expect(wrapper.find("iframe")).toHaveLength(5);
   });
-  test("test to see if render 1 List component in Pods page", () => {
+  test("render 1 List component in Pods page", () => {
     expect(wrapper.find("List")).toHaveLength(1);
   });
-  test("test to see if there are 6 list headers passed to List component", () => {
+  test("6 list headers passed to List component", () => {
     console.log(wrapper.find("List").props().listValueHeaders);
     expect(wrapper.find("List").props().listValueHeaders).toHaveLength(6);
   });
-  test("test to see if list reroute prop is truthy", () => {
+  test("list reroute prop is truthy", () => {
     console.log(wrapper.find("List").props().reroute);
     expect(wrapper.find("List").props().reroute).toBeTruthy();
   });
